a477d36b48961523f948fc91ea3697299b617ef7
Jack <12833246+jackgoffinet@users.noreply.github.com>
2022-09-06 12:14:15 -0400

    fixed bug in test_models

--- test/test_models.py
+++ test/test_models.py
@@ -15,16 +15,15 @@ from lpne.models import FaSae, CpSae
 
 def test_factor_analysis_sae():
     """Make some fake data, train briefly, and reload the model."""
-    # Set up fake data.
-    n = 100 # number of datapoints/windows
-    n_features = 100 # total number of LFP features
-    n_classes = 3 # number of label types
-    features = np.random.randn(n, n_features)
-    labels = np.random.randint(n_classes, size=n)
+    b, f, r, g = 10, 9, 8, 2
+    features = np.random.randn(b, f, r, r)
+    labels = np.random.randint(0, 3, size=(b,))
+    groups = np.random.randint(0, g, size=(b,))
+
     # Run through different combinations of the model.
-    nonnegative_vals = [True, False]
+    nmf_vals = [True, False]
     variational_vals = [True, False]
-    for nonnegative, variational in product(nonnegative_vals, variational_vals):
+    for nonnegative, variational in product(nmf_vals, variational_vals):
         # Make the model.
         model = FaSae(
                 nonnegative=nonnegative,
@@ -32,13 +31,14 @@ def test_factor_analysis_sae():
                 n_iter=1,
         )
         # Fit the model.
-        model.fit(features, labels, print_freq=None)
+        model.fit(features, labels, groups, print_freq=None)
         # Make some predictions.
         predictions = model.predict(features)
         # Calculate a weighted accuracy.
         weighted_acc_orig = model.score(
                 features,
                 labels,
+                groups,
         )
         # Get state.
         params = model.get_params()
@@ -49,6 +49,7 @@ def test_factor_analysis_sae():
         weighted_acc = new_model.score(
                 features,
                 labels,
+                groups,
         )
         assert weighted_acc_orig == weighted_acc
 
@@ -58,11 +59,10 @@ def test_cp_sae():
     features = np.random.randn(b, f, r, r)
     labels = np.random.randint(0, 3, size=(b,))
     groups = np.random.randint(0, g, size=(b,))
-    weights = np.exp(np.random.randn(b))
     model = CpSae(n_iter=1)
     model.fit(features, labels, groups, print_freq=None)
     # Make some predictions.
-    predictions = model.predict(features, groups)
+    _ = model.predict(features, groups)
     # Calculate a weighted accuracy.
     weighted_acc_orig = model.score(
             features,
